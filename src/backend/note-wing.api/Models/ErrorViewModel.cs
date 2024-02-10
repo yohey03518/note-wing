255e6a11c8627c38d4830b6c288aa7232e6b0434
erwin <yohey03518@gmail.com>
2024-02-10 17:57:01 +0800

    clean up useless file

--- src/backend/note-wing.api/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace note_wing.api.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
