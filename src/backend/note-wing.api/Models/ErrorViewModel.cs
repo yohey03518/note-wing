3608706d315da1f0a3bd1fe6fd91f0de370fe328
erwin <yohey03518@gmail.com>
2024-02-10 13:52:21 +0800

    init backend

---
+++ src/backend/note-wing.api/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace note_wing.api.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
